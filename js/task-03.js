85f052e5c42ab1e0a113731d7a0ae2fdc0227ab5
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-02 20:43:41 +0300

    Add task 03

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,38 @@
-"use strict";
+'use strict';
+class Storage {
+  constructor(input) {
+    this.items = input;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      this.items.splice(items.indexOf(item), 1);
+    } else {
+      console.log('This item cannot be deleted because it was not found');
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items);
+
+storage.addItem('Дроид');
+console.table(storage.items);
+
+storage.removeItem('Пролонгер');
+console.table(storage.items);
+
+storage.removeItem('Пролонгер');
+console.table(storage.items);
