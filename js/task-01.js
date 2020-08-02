6b94e442866627254d3c6a3cc0415afb6ed3fc1c
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-08-02 19:54:49 +0300

    Add task 02

--- js/task-01.js
+++ js/task-01.js
@@ -13,10 +13,12 @@ const mango = new Account({
   login: 'Mangozedog',
   email: 'mango@dog.woof',
 });
+
 mango.getInfo();
 
 const poly = new Account({
   login: 'Poly',
   email: 'poly@mail.com',
 });
+
 poly.getInfo();
--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,30 @@
-"use strict";
+'use strict';
+
+class User {
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.followers = obj.followers;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`,
+    );
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo();
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo();
